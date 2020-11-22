00b65a37187fab7eeb3fb1f95819e4e22a11c48c
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2020-11-22 09:02:33 +0900

    整列

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -156,127 +156,125 @@ class _EditScreenState extends State<EditScreen> {
       return;
     }
 
-    showDialog(context: context, builder: (_) =>
-        AlertDialog(
-          title: Text("登録"),
-          content: Text("登録していいですか？"),
-          actions: [
-            FlatButton(
-              child: Text("はい"),
-              onPressed: () async {
-                var word = Word(
-                    strQuestion: questionController.text,
-                    strAnswer: answerController.text);
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              title: Text("登録"),
+              content: Text("登録していいですか？"),
+              actions: [
+                FlatButton(
+                  child: Text("はい"),
+                  onPressed: () async {
+                    var word = Word(
+                        strQuestion: questionController.text,
+                        strAnswer: answerController.text);
 
-                try {
-                  await dataBase.addWord(word);
-                  print("OK");
-                  questionController.clear();
-                  answerController.clear();
-                  //TODO　登録完了メッセージ
-                  Toast.show("登録完了しました。", context, duration: Toast.LENGTH_LONG);
-                } on SqliteException catch (e) {
-                  Toast.show("この問題は既に登録されています。", context,
-                      duration: Toast.LENGTH_LONG);
-                } finally {
-                  Navigator.pop(context);
-                }
-              },
-            ),
+                    try {
+                      await dataBase.addWord(word);
+                      print("OK");
+                      questionController.clear();
+                      answerController.clear();
+                      //TODO　登録完了メッセージ
+                      Toast.show("登録完了しました。", context,
+                          duration: Toast.LENGTH_LONG);
+                    } on SqliteException catch (e) {
+                      Toast.show("この問題は既に登録されています。", context,
+                          duration: Toast.LENGTH_LONG);
+                    } finally {
+                      Navigator.pop(context);
+                    }
+                  },
+                ),
+                FlatButton(
+                    child: Text("いいえ"), onPressed: () => Navigator.pop(context))
+              ],
+            ));
 
-            FlatButton(
-                child: Text("いいえ"),
-                onPressed: () => Navigator.pop(context)
-            )
-          ],
-
-        ));
-
-
-    void _updateWord () async {
+    void _updateWord() async {
       if (questionController.text == "" || answerController.text == "") {
         Toast.show("問題と答えの両方を入力しないと登録できませ。", context,
             duration: Toast.LENGTH_LONG);
         return;
       }
 
-      showDialog(context: context,builder: (_) => AlertDialog(
-        title: Text("${questionController.text}の変更"),
-        content: Text("変更してもいいですか？"),
-        actions: [
-          FlatButton(
-          child: Text("はい"),
-      onPressed: () async {
-        var word = Word(
-            strQuestion: questionController.text,
-            strAnswer: answerController.text,
-            isMemorized: false);
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                title: Text("${questionController.text}の変更"),
+                content: Text("変更してもいいですか？"),
+                actions: [
+                  FlatButton(
+                    child: Text("はい"),
+                    onPressed: () async {
+                      var word = Word(
+                          strQuestion: questionController.text,
+                          strAnswer: answerController.text,
+                          isMemorized: false);
 
-        try {
-          await dataBase.updateWord(word);
-          _backToWordListScreen();
-          Toast.show("修正が完了しました。", context, duration: Toast.LENGTH_LONG);
-        } on SqliteException catch (e) {
-          Toast.show("何らかの問題が発生し登録できません。: $e", context,
-              duration: Toast.LENGTH_LONG);
-          return;
-        } finally {
-          Navigator.pop(context);
-         }
-        },
-       ),
-        FlatButton(
-        child: Text("いいえ"),
-        onPressed: () => Navigator.pop(context),
-      )
+                      try {
+                        await dataBase.updateWord(word);
+                        _backToWordListScreen();
+                        Toast.show("修正が完了しました。", context,
+                            duration: Toast.LENGTH_LONG);
+                      } on SqliteException catch (e) {
+                        Toast.show("何らかの問題が発生し登録できません。: $e", context,
+                            duration: Toast.LENGTH_LONG);
+                        return;
+                      } finally {
+                        Navigator.pop(context);
+                      }
+                    },
+                  ),
+                  FlatButton(
+                    child: Text("いいえ"),
+                    onPressed: () => Navigator.pop(context),
+                  )
+                ],
+              ));
+    }
+  }
 
-        ],
-      ));
-
-
-      }
+  void _updateWord() {
+    if (questionController.text == "" || answerController.text == "") {
+      Toast.show("問題と答えの両方を入力しないと登録できませ。", context,
+          duration: Toast.LENGTH_LONG);
+      return;
     }
 
-  void _updateWord() {if (questionController.text == "" || answerController.text == "") {
-    Toast.show("問題と答えの両方を入力しないと登録できませ。", context,
-        duration: Toast.LENGTH_LONG);
-    return;
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              title: Text("${questionController.text}の変更"),
+              content: Text("変更してもいいですか？"),
+              actions: [
+                FlatButton(
+                  child: Text("はい"),
+                  onPressed: () async {
+                    var word = Word(
+                        strQuestion: questionController.text,
+                        strAnswer: answerController.text,
+                        isMemorized: false);
+
+                    try {
+                      await dataBase.updateWord(word);
+                      Navigator.pop(context);
+
+                      _backToWordListScreen();
+                      Toast.show("修正が完了しました。", context,
+                          duration: Toast.LENGTH_LONG);
+                    } on SqliteException catch (e) {
+                      Toast.show("何らかの問題が発生し登録できません。: $e", context,
+                          duration: Toast.LENGTH_LONG);
+
+                      Navigator.pop(context);
+                    }
+                  },
+                ),
+                FlatButton(
+                  child: Text("いいえ"),
+                  onPressed: () => Navigator.pop(context),
+                )
+              ],
+            ));
   }
-
-  showDialog(context: context,builder: (_) => AlertDialog(
-    title: Text("${questionController.text}の変更"),
-    content: Text("変更してもいいですか？"),
-    actions: [
-      FlatButton(
-        child: Text("はい"),
-        onPressed: () async {
-          var word = Word(
-              strQuestion: questionController.text,
-              strAnswer: answerController.text,
-              isMemorized: false);
-
-          try {
-            await dataBase.updateWord(word);
-            Navigator.pop(context);
-
-            _backToWordListScreen();
-            Toast.show("修正が完了しました。", context, duration: Toast.LENGTH_LONG);
-          } on SqliteException catch (e) {
-            Toast.show("何らかの問題が発生し登録できません。: $e", context,
-                duration: Toast.LENGTH_LONG);
-            Navigator.pop(context);
-
-          }
-        },
-      ),
-      FlatButton(
-        child: Text("いいえ"),
-        onPressed: () => Navigator.pop(context),
-      )
-
-    ],
-  ));
-
-
-  }
-  }
+}
